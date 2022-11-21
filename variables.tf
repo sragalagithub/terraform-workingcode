d786755169f6e6171cd971d37ce211c9c5caf739
Nithin <47190129+nithin44@users.noreply.github.com>
2022-11-21 16:24:45 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,28 @@
 variable "region" {
-    type = string
-    default = "us-central"
+  type    = string
+  default = "us-central"
 }
 variable "project" {
-    type = string
+  type = string
 }
 
 variable "user" {
-    type = string
+  type = string
 }
 
 variable "email" {
-    type = string
+  type = string
 }
 variable "privatekeypath" {
-    type = string
-    default = "~/.ssh/id_rsa"
+  type    = string
+  default = "C:\\Users\\NUppalapati\\.ssh\\id_rsa"
 }
 
+variable "cmek_crypto_key" {
+  type = string
+  default = "projects/vpc-host-nonprod-xd806-rw998/locations/us-central1/keyRings/shared-citco-keyring/cryptoKeys/shared-citco-key"
+}
 variable "publickeypath" {
-    type = string
-    default = "~/.ssh/id_rsa.pub"
+  type    = string
+  default = "C:\\Users\\NUppalapati\\.ssh\\id_rsa.pub"
 }
